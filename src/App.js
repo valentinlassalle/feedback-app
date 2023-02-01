4fd21e4d1b3d37a164092288b920799010a8d2df
Valentin LASSALLE <valentinlassalle@hotmail.com>
2023-02-01 15:15:27 +0100

    feat: back to normal

--- src/App.js
+++ src/App.js
@@ -1,36 +1,8 @@
 function App() {
-    const title = 'Blog Post'
-    const body = 'This is my blog post'
-    const comments = [
-        {id: 1, text: 'Comment one'},
-        {id: 2, text: 'Comment two'},
-        {id: 3, text: 'Comment three'},
-    ]
-
-    const loading = false
-    const showComments = true
-    const commentBlock =  (
-        <div className="comments">
-            <h3>Comments ({comments.length})</h3>
-            <ul>
-                {comments.map((comment, index) => (
-                    <li key={index}>{comment.text}</li>
-                ))}
-            </ul>
-        </div>
-    )
-
-    if(loading) return <h1>holding...</h1>
-
     return (
-    <div className='container'>
-        <h1>{title}</h1>
-        <p>{body}</p>
-        
-        {showComments && commentBlock}
-    </div>
-        
+        <h1>My App</h1>
     )
+
 }
 
 export default App
