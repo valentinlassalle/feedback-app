6a06360d0e4cf51abb8704b781264f78688bd3a8
Valentin LASSALLE <valentinlassalle@hotmail.com>
2023-02-01 15:46:37 +0100

    feat: first component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
+import Header from "./components/Header"
+
 function App() {
     return (
-    <div className='container'>
-        <h1>My App</h1>
-        
-    </div>
+        <>
+            <Header  text="Hello World" />
+            <div className="container">
+                <h1>My App</h1>
+            </div>
+        </>
         
     )
 }
