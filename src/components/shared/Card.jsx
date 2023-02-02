273bc05d83dd2ed0a093743cd7a817052b595f83
Valentin LASSALLE <valentinlassalle@hotmail.com>
2023-02-02 15:12:57 +0100

    feat: array of cards and conditional styling

---
+++ src/components/shared/Card.jsx
@@ -0,0 +1,17 @@
+import PropTypes from 'prop-types'
+
+function Card({children, reverse}) {
+    return (<div className={`card ${reverse && 'reverse'}`}>{children}</div>)
+
+    //return <div className="card" style={{backgroundColor: reverse ? 'rgba(0,0,0,0.4)' : '#fff', color: reverse ? '#fff' : '#000',}}> {children}</div>
+}
+
+Card.defaultProps = {
+    reverse: false,
+}
+
+Card.propTypes = {
+    reverse: PropTypes.bool,
+    children: PropTypes.node.isRequired,
+}
+export default Card
